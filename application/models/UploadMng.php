6b2930adbed4814b66ddd47ed2fc5bfd1a6ffda7
ljianhui <ljianhui2012@163.com>
2016-08-09 00:39:20 +0800

    add upload manager

--- application/models/UploadMng.php
+++ application/models/UploadMng.php
@@ -2,6 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UploadMng extends CI_Model {
+
+	private $str_err_msg;
+
 	public function UploadMng()
 	{
 		parent::__construct();
@@ -9,15 +12,21 @@ class UploadMng extends CI_Model {
 		$config['upload_path']      = './uploads/';
         $config['allowed_types']    = 'gif|jpg|png';
         $config['max_size']     	= 2048;
-        $config['max_width']        = 1024;
-        $config['max_height']       = 1024;
+        $config['max_width']        = 2048;
+        $config['max_height']       = 2048;
         $this->load->library('upload', $config);
+	}
+
+	public function get_err_msg()
+	{
+		return $this->str_err_msg;
 	}
 
 	public function upload_cover_img()
 	{	
 		if (!$this->upload->do_upload('coverimg'))
 		{
+			$str_err_msg = strip_tags($this->upload->display_errors());
 			return false;
 		}
 
@@ -27,6 +36,7 @@ class UploadMng extends CI_Model {
 		$ret = rename($data['full_path'], $coverimg_path.$coverimg_name);
 		if ($ret === false)
 		{
+			$str_err_msg = 'rename failed';
 			return false;
 		}
 
@@ -37,6 +47,7 @@ class UploadMng extends CI_Model {
 	{
 		if (!$this->upload->do_upload('headimg'))
 		{
+			$str_err_msg = strip_tags($this->upload->display_errors());
 			return false;
 		}
 
@@ -46,6 +57,7 @@ class UploadMng extends CI_Model {
 		$ret = rename($data['full_path'], $headimg_path.$headimg_name);
 		if ($ret === false)
 		{
+			$str_err_msg = 'rename failed';
 			return false;
 		}
 
@@ -56,6 +68,7 @@ class UploadMng extends CI_Model {
 	{
 		if (!$this->upload->do_upload('articleimg'))
 		{
+			$str_err_msg = strip_tags($this->upload->display_errors());
 			return false;
 		}
 
@@ -65,9 +78,31 @@ class UploadMng extends CI_Model {
 		$ret = rename($data['full_path'], $articleimg_path.$articleimg_name);
 		if ($ret === false)
 		{
+			$str_err_msg = 'rename failed';
 			return false;
 		}
 
 		return '/imgs/article/'.$articleimg_name;
+	}
+
+	public function upload_carousel_img()
+	{
+		if (!$this->upload->do_upload('carouselimg'))
+		{
+			$str_err_msg = strip_tags($this->upload->display_errors());
+			return false;
+		}
+
+		$data = $this->upload->data();
+		$articleimg_name = 'carousel_'.date('YmdHis', time()).strval(mt_rand(100000, 999999)).$data['file_ext'];
+		$articleimg_path = $data['file_path'].'../imgs/carousel/';
+		$ret = rename($data['full_path'], $articleimg_path.$articleimg_name);
+		if ($ret === false)
+		{
+			$str_err_msg = 'rename failed';
+			return false;
+		}
+
+		return '/imgs/carousel/'.$articleimg_name;
 	}
 }
